7792d139196dd25b21d47ead62a755ba63a0061c
elij <elij.mx@gmail.com>
2010-05-30 14:16:44 -0700

    more output changes

--- scripts/create_links.bash
+++ scripts/create_links.bash
@@ -6,12 +6,12 @@ for dotfile in ${NDIR}/dotfiles/*; do
     FFILE="$(basename "${dotfile}")"
     OFILE="${HOME}/${FFILE}"
     if [ -h "${OFILE}" ]; then
-        echo "x> ${OFILE} is already linked"
+        echo "x> ${OFILE} is already linked" >&2
     elif [ -f "${OFILE}" ] || [ -d "${OFILE}" ]; then
-        echo "-> ${OFILE} already exists, but is not a link"
-        echo "   remove file and run again to link"
+        echo "--> ${OFILE} already exists, but is not a link" >&2
+        echo "   remove file and run again to link" >&2
     else
-        echo "o> linking ${OFILE} to ${dotfile}"
+        echo "o> linking ${OFILE} to ${dotfile}" >&2
         ln -s "${dotfile}" "${OFILE}"
     fi
 done
