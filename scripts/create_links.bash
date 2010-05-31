7634e6594d933aa000b375ad76037fa26339efed
elij <elij.mx@gmail.com>
2010-05-31 16:55:40 -0700

    remove debug logging on 'everything ok'

--- scripts/create_links.bash
+++ scripts/create_links.bash
@@ -6,7 +6,9 @@ for dotfile in ${NDIR}/dotfiles/*; do
     FFILE="$(basename "${dotfile}")"
     OFILE="${HOME}/${FFILE}"
     if [ -h "${OFILE}" ]; then
-        echo "-> ${OFILE} is already linked" >&2
+        ## just run silently if it is already a symlink
+        #echo "-> ${OFILE} is already linked" >&2
+        continue
     elif [ -f "${OFILE}" ] || [ -d "${OFILE}" ]; then
         echo "x> ${OFILE} already exists, but is not a link" >&2
         echo "     remove file and run again to link" >&2
