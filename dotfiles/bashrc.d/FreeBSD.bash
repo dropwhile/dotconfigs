b7d64ee53201c2113022b462dc8483454563d0ef
elij <elij@wundrd.net>
2014-07-16 10:28:43 -0700

    minor updates

--- dotfiles/bashrc.d/FreeBSD.bash
+++ dotfiles/bashrc.d/FreeBSD.bash
@@ -1,3 +1,8 @@
 export LSCOLORS='gxfxcxdxbxegedabagacad'
 export BLOCKSIZE=K;
 alias psf='ps wwfaux'
+## enable bash completion, but only if it didn't get loaded by
+## profile.d scripts or something
+[[ $PS1 && -z "${BASH_COMPLETION}" && \
+    -f /usr/local/share/bash-completion/bash_completion.sh ]] && \
+    source /usr/local/share/bash-completion/bash_completion.sh
