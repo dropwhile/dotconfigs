4586facfe29dc7ca1ed8127d915c371a96f7d3e9
elij <elij@wundrd.net>
2017-05-04 14:44:50 -0700

    rename scripts

--- scripts/dotfiles_update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd $HOME/.myconfigs/ && git pull && $HOME/.myconfigs/scripts/create_links.sh
---
+++ scripts/update.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd $HOME/.myconfigs/ && git pull && $HOME/.myconfigs/scripts/setup.sh
