1658e8db41858cef62814e79943fba81ea21ad96
elij <elij@wundrd.net>
2019-08-19 00:25:04 -0700

    fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $HOME/.myconfigs/ && git pull && $HOME/.myconfigs/scripts/setup.sh
 
-if type -p zsh &>/dev/null; then
+if type zsh 2>/dev/null; then
  $HOME/.myconfigs/scripts/recompile.zsh
 fi
