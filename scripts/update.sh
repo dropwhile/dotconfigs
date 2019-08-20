24293e73d5ed085f20db7b446fe276e862a1265e
elij <elij@wundrd.net>
2019-08-20 14:38:09 -0700

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,3 +4,6 @@ cd $HOME/.myconfigs/ && git pull && $HOME/.myconfigs/scripts/setup.sh
 if type zsh >/dev/null 2>&1; then
  $HOME/.myconfigs/scripts/recompile.zsh
 fi
+
+## cleanups
+rm -f ${HOME}/.profile.d
