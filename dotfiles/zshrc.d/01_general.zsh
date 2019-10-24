596ef5fc11350e29188e823b649cd194e6a25711
elij <elij@wundrd.net>
2019-10-24 02:12:58 -0700

    tweak histsize a bit

--- dotfiles/zshrc.d/01_general.zsh
+++ dotfiles/zshrc.d/01_general.zsh
@@ -55,8 +55,8 @@ setopt hist_ignore_space hist_no_functions hist_no_store hist_reduce_blanks
 setopt inc_append_history
 HISTORY_IGNORE="(l[lsa]|[bf]g|cd *|pushd *|popd *|exit|clear|reset)"
 HISTFILE=~/.zsh_history
-HISTSIZE=2000
-SAVEHIST=4000
+HISTSIZE=2100
+SAVEHIST=2000
 
 
 ##########################
