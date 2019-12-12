99f7adb3ef9ef7f7e358d985ac24d6991f9a1808
elij <elij@wundrd.net>
2019-12-12 13:36:33 -0800

    a bit cleaner

--- dotfiles/zshrc.d/04_completion.zsh
+++ dotfiles/zshrc.d/04_completion.zsh
@@ -7,9 +7,8 @@ zstyle ':compinstall' filename '$HOME/.zshrc'
 zcachedir="$HOME/.cache/zsh"
 [[ -d "$zcachedir" ]] || mkdir -p "$zcachedir"
 
-_update_zcomp() {
-    setopt local_options
-    setopt extendedglob
+() {
+    setopt local_options extendedglob
     autoload -Uz compinit
     local zcompf="$1/zcompdump"
     # use a separate file to determine when to regenerate, as compinit doesn't
@@ -36,9 +35,7 @@ _update_zcomp() {
         # run in background
         zcompile -M "$zcompf" &!
     fi
-    unfunction _update_zcomp
-}
-_update_zcomp "$zcachedir"
+} "$zcachedir"
 
 zcompcdir="$zcachedir/zcompcache"
 [[ -d "$zcompcdir" ]] || mkdir -p "$zcompcdir"
@@ -80,12 +77,10 @@ zstyle ':completion:*' users
 #zstyle ':completion:*' group-name ''
 
 # Use ls-colors for path completions
-function _set-list-colors() {
+() {
      local LS_COLORS='no=00:fi=00:ca=00:di=36:ln=35:pi=33:so=36:bd=40;33;01:cd=40;33;01:or=35:mi=41;30;01:ex=31:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tz=01;31:*.rpm=01;31:*.cpio=01;31:'
 	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-	unfunction _set-list-colors
 }
-_set-list-colors
 
 # list dirs first
 zstyle ':completion:*' list-dirs-first true
