96e13da16de7045b1d80ebb98278d18cf70e1858
elij <elij@wundrd.net>
2020-01-24 03:18:50 -0800

    Revert "avoid some work"
    This reverts commit 785b0af9d4930cd3a80cb983da547e2e12e496a7.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,24 +1,9 @@
 #!/bin/sh
+cd $HOME/.myconfigs/ && git pull && $HOME/.myconfigs/scripts/setup.sh
 
-if [ ! -d $HOME/.myconfigs ]; then
-    echo "No ~/.myconfigs... Giving up!"
-    exit 1
-fi
-
-# default to out of date
-UP_TO_DATE=1
-
-cd $HOME/.myconfigs/
-git status -v|grep -q 'is up to date'
-UP_TO_DATE=$?
-[ $UP_TO_DATE -ne 0 ] && git pull
-
-cd $HOME
-$HOME/.myconfigs/scripts/setup.sh
-
-if [ $UP_TO_DATE -ne 0 ] && type zsh >/dev/null 2>&1; then
-    $HOME/.myconfigs/scripts/recompile.zsh
+if type zsh >/dev/null 2>&1; then
+ $HOME/.myconfigs/scripts/recompile.zsh
 fi
 
 ## cleanups
-#rm -f ${HOME}/.profile.d
+rm -f ${HOME}/.profile.d
