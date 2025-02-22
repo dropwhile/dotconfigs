9c35f89f5881191bfbcd342cd2a019e6cdfcff50
elij <elij@wundrd.net>
2025-02-22 12:57:00 -0800

    make chimera more it's own OS

---
+++ dotfiles/zshrc.d/os/linux-chimera.zsh
@@ -0,0 +1,9 @@
+# chimera uses freebsd userspace, so this is based on freebsd.zsh
+export LSCOLORS='gxfxcxdxbxegedabagacad'
+export BLOCKSIZE=K;
+
+# enable colors in grep
+export GREP_OPTIONS='--color=auto'
+
+alias psf='ps wwfaux'
+alias ls='ls -p'
