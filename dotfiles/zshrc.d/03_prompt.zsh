6e1917212517e935804aa67b8618827ccedcae7f
elij <elij@wundrd.net>
2025-09-07 17:25:08 -0700

    update

--- dotfiles/zshrc.d/03_prompt.zsh
+++ dotfiles/zshrc.d/03_prompt.zsh
@@ -31,10 +31,26 @@ if [[ $hname == '' && $upart == '' && $IS_SSH -ne 1 ]]; then
     ps_c=''
 fi
 
+# myprecmd() {
+#     # Print a newline before the prompt, unless it's the
+#     # first prompt in the process.
+#     if [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+#         print ""
+#     fi
+#     if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+#         NEW_LINE_BEFORE_PROMPT=1
+#     fi
+# }
+#
+# autoload -Uz add-zsh-hook
+# add-zsh-hook precmd myprecmd
+# alias clear="unset NEW_LINE_BEFORE_PROMPT && clear"
+# alias reset="unset NEW_LINE_BEFORE_PROMPT && reset"
+
 #PS1=${ps_c}'%n@'${hname}'%f:%1~%(!.%F{red}.%f)${vcs_info_msg_0_}%#%f%E '
 #PS1=${ps_c}'%n@'${hname}'%f:%1~%(!.%F{red}.%f)${vcs_info_msg_0_}%#%f '
 #PS1=${ps_c}'%n@'${hname}'%f:%1~%(!.%F{red}.%f)%#%f '
 #PS1=${ps_c}${upart}${hname}'%f'${ccolon}'%1~%(!.%F{red}.%f)%#%f '
-PS1=${ps_c}${upart}${hname}'%f'${ccolon}'%1~%(?..[%F{red}%?%f])%(!.%F{red}.%f)%#%f '
-RPS1='${vcs_info_msg_0_}'
+PS1='┌─ '${ps_c}${upart}${hname}'%f'${ccolon}'%1~%(?..[%F{red}%?%f])%(!.%F{red}.%f) ${vcs_info_msg_0_}'$'\n''└─ %#%f '
+#RPS1='${vcs_info_msg_0_}'
 unset ps_c hname upart ccolon TWO_DOT_HOSTNAME
